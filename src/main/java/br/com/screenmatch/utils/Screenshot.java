eebc40ae11932f62b92e76d44a8a5fd54c9428b0
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2023-09-12 13:58:59 -0300

    Criando a funcionalidade de editar filmes

--- src/main/java/br/com/screenmatch/utils/Screenshot.java
+++ src/main/java/br/com/screenmatch/utils/Screenshot.java
@@ -13,21 +13,46 @@ import java.util.Date;
 
 public class Screenshot {
 
-
-
-    public static void takeScreenShot(WebDriver driver) {
-        try {
-            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-             FileUtils.copyFile(screenshotFile, new File(fileNameGenerator()));
-        } catch (IOException e) {
-            throw new RuntimeException("Não foi possível capturar a tela");
+        public static void takeScreenShot(WebDriver driver) {
+            try {
+                File screenshotFile = captureScreenshot(driver);
+                saveScreenshot(screenshotFile);
+            } catch (IOException e) {
+                throw new RuntimeException("Não foi possível capturar a tela", e);
+            }
         }
-    }
 
-    private static String fileNameGenerator() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HH:mm");
-        String path = "src/test/resources/screenshots/";
-        return path + "evidencia-" + formatter.format(new Date()) + ".png";
-    }
+        private static File captureScreenshot(WebDriver driver) throws IOException {
+            return ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        }
+
+        private static void saveScreenshot(File screenshotFile) throws IOException {
+            String fileName = generateFileName();
+            File destFile = new File(fileName);
+            FileUtils.copyFile(screenshotFile, destFile);
+        }
+
+        private static String generateFileName() {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HH:mm");
+            String path = "src/test/resources/screenshots/";
+            return path + "evidencia-" + formatter.format(new Date()) + ".png";
+        }
+
+
+
+//    public static void takeScreenShot(WebDriver driver) {
+//        try {
+//            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//             FileUtils.copyFile(screenshotFile, new File(fileNameGenerator()));
+//        } catch (IOException e) {
+//            throw new RuntimeException("Não foi possível capturar a tela");
+//        }
+//    }
+//
+//    private static String fileNameGenerator() {
+//        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HH:mm");
+//        String path = "src/test/resources/screenshots/";
+//        return path + "evidencia-" + formatter.format(new Date()) + ".png";
+//    }
 
 }
